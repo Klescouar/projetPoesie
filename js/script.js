bb2e40a569248cf5d28f19b71e82f86be0bec0f6
Kevin Le Scouarnec <lescouarneckevin@yahoo.fr>
2016-05-12 15:35:22 +0200

    Catégories OK

--- js/script.js
+++ js/script.js
@@ -22,26 +22,48 @@ app.controller('poesieCtrl', ["$scope", "$http", function($scope, $http) {
                 }
 
                 if ($scope.categories[0].status == true) {
-                    for (var i = 0; i < $scope.dico[word].synonymes.length; i++) {
-                        $scope.syno.push($scope.dico[word].synonymes[i]);
-                        console.log($scope.syno);
+                    if ($scope.dico[word].synonymes) {
+                        for (var i = 0; i < $scope.dico[word].synonymes.length; i++) {
+                            $scope.syno.push($scope.dico[word].synonymes[i]);
+                            console.log($scope.syno);
+                        }
+                    } else {
+                        $scope.syno.push("Pas de synonymes disponibles pour ce mot")
                     }
                 }
+
                 if ($scope.categories[2].status == true) {
-                    for (var i = 0; i < $scope.dico[word].anagrammes.length; i++) {
-                        $scope.ana.push($scope.dico[word].anagrammes[i]);
+                    if ($scope.dico[word].anagrammes) {
+                        for (var i = 0; i < $scope.dico[word].anagrammes.length; i++) {
+                            $scope.ana.push($scope.dico[word].anagrammes[i]);
+                        }
+                    } else {
+                        $scope.ana.push("Pas d'anagrammes disponibles pour ce mot")
                     }
                 }
 
                 if ($scope.categories[1].status == true) {
                     var numPoint = $scope.dico[word].phonetique.lastIndexOf(".");
                     var lastSyl = $scope.dico[word].phonetique.slice(numPoint, $scope.dico[word].phonetique.length);
-                    for (var mots in $scope.dico) {
-                        var numPointDico = $scope.dico[mots].phonetique.lastIndexOf(".");
-                        var lastSylDico = $scope.dico[mots].phonetique.slice(numPointDico, $scope.dico[mots].phonetique.length);
-                        if (lastSyl === lastSylDico && word !== mots) {
-                            $scope.rimes.push(mots)
+                    if (numPoint !== -1) {
+                        for (var mots in $scope.dico) {
+                            var numPointDico = $scope.dico[mots].phonetique.lastIndexOf(".");
+                            var lastSylDico = $scope.dico[mots].phonetique.slice(numPointDico, $scope.dico[mots].phonetique.length);
+                            if (lastSyl === lastSylDico && word !== mots) {
+                                $scope.rimes.push(mots)
+                            }
                         }
+                    } else {
+                        for (var mots in $scope.dico) {
+                            var numPointDico = $scope.dico[mots].phonetique.lastIndexOf(".");
+                            var lastSylDico = $scope.dico[mots].phonetique.slice(numPointDico, $scope.dico[mots].phonetique.length);
+                            if (($scope.dico[word].phonetique === $scope.dico[mots].phonetique || $scope.dico[word].phonetique === lastSylDico) && word !== mots) {
+                                $scope.rimes.push(mots)
+                            }
+                        }
+                    }
+                    if ($scope.rimes.length === 0) {
+                      $scope.rimes.push("Pas de rime disponibles pour ce mot  ")
                     }
                 }
                 if ($scope.categories[3].status == true) {
@@ -49,6 +71,9 @@ app.controller('poesieCtrl', ["$scope", "$http", function($scope, $http) {
                         if ($scope.dico[word].phonetique === $scope.dico[mots].phonetique && word !== mots) {
                             $scope.homo.push(mots)
                         }
+                    }
+                    if ($scope.homo.length === 0) {
+                        $scope.homo.push("Pas d'homophones disponibles pour ce mot")
                     }
                 };
             };
